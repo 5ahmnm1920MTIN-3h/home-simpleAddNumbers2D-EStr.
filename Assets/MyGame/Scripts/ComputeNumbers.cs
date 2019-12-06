6c4dcbb03662908b52e40f84418d19ab253d90be
EStr <elisa.stranzinger@htl-salzburg.ac.at>
2019-12-06 17:26:15 +0100

    initialised Project, set up Unity, updated gitignore, add GUI in Unity, added Script adding Numbers, Error - FormatException

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public InputField ipVarA;
+    public InputField ipVarB;
+    public Text result;
+
+    float varA;
+    float varB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetResult();
+    }
+
+    public void SetResult()
+    {
+        float a = float.Parse(ipVarA.text);
+        float b = float.Parse(ipVarB.text);
+        Debug.Log("in Result" + (a + b));
+    }
+
+    public float AddNumbers(float variableA, float variableB)
+    {
+        float result = variableA + variableB;
+        return result;
+    }
+}
